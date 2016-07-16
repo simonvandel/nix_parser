497397474a0ddce3009f0686308fb7513e880d79
Simon Vandel Sillesen <simon.vandel@gmail.com>
2016-07-16 22:07:38 +0200

    add python script to check validity of nix test cases

---
+++ valid_test_cases.py
@@ -0,0 +1,12 @@
+# Tested with Python 3.5
+# This script checks all nix test cases against the reference nix parser to check for its validity
+
+import glob
+import subprocess
+import sys
+
+nix_files = glob.iglob("test_cases/**/*.nix", recursive=True)
+for nix_file in nix_files:
+    completed_process = subprocess.run(["nix-instantiate", "--parse", nix_file])
+    if completed_process.returncode != 0:
+        sys.exit("ERROR: " + nix_file + " does not seem to be a valid Nix file!")
